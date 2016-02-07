db8547620f8c34ca2ba2de83fc28eb84068d354d
Charles Gruenwald <charles@thinktopic.com>
2016-02-07 21:43:20 +0000

    [webapp] Resize using a canvas rather than code.

--- src/cljs/browser_mnist/core.cljs
+++ src/cljs/browser_mnist/core.cljs
@@ -82,7 +82,7 @@
 (defn mean [coll]
   (/ (apply + coll) (count coll)))
 
-(defn get-array []
+(defn get-array-old []
   (let [out-width 28
         out-height 28
         rows (->> (array-seq (canvas/get-pixels) 0)
@@ -104,6 +104,27 @@
       (println "[" (map #(format "% 2.2f" %) r) "]"))
     (classify pixels)))
 
+(defn get-array []
+  (let [small-canvas (.getElementById js/document "small-canvas")
+        small-context (.getContext small-canvas "2d")
+        _ (doto small-context
+            (.clearRect 0 0 28 28)
+            (.drawImage (canvas/get-canvas) 0 0 28 28))
+        pixels (-> small-context
+                   (.getImageData 0 0 28 28)
+                   (.-data)
+                   (array-seq 0))
+        pixels-ary (->> pixels
+                        (partition 4)        ; array -> rgba
+                        (map #(take 3 %))    ; drop the alpha
+                        (mapv (fn [[r g b]] (/ (+ (* 0.3 r) (* 0.59 g) (* 0.11 b)) 255.0))) ; greyscale
+                        )]
+    (classify pixels-ary)
+    (println "count of pixel ary: " (count pixels-ary))
+    ;(doseq [r (m/to-nested-vectors pixels-ary)]
+    ;  (println "[" (map #(format "% 2.2f" %) r) "]"))
+    ;(classify pixels)
+    ))
 
 ;; -------------------------
 ;; Page
@@ -111,11 +132,17 @@
 (defn home-page []
   [:div [:h2 "Welcome to browser-mnist"]
    [:div
-    [canvas/canvas]]
-   [:div {:style btn-style :on-click #(do (canvas/clear!) (reset! classified* nil))} "Clear"]
-   [:div {:style btn-style :on-click #(get-array)} "Recognize"]
+    [canvas/canvas]
+    [:canvas {:id "small-canvas"
+              :style {:display "none"
+                      :border "1px solid blue"
+                      :background-color "black"}
+              :width "28"
+              :height "28"}]]
+   [:span {:style btn-style :on-click #(do (canvas/clear!) (reset! classified* nil))} "Clear"]
+   [:span {:style btn-style :on-click #(get-array)} "Recognize"]
    (when @classified*
-     [:div "Result: " @classified*])])
+     [:span {:style {:display "inline-block"}} " Result: " @classified*])])
 
 ;; -------------------------
 ;; Routing
